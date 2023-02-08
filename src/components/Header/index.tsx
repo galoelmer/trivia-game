53a9bea523ef53bbb0bd63329d894dae9ee1749a
Elmer Galo <elmer.galo@outlook.com>
2023-02-08 18:15:15 -0500

    initial app setup

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const Header: React.FC = () => {
+  return (
+    <View>
+      <Text>Trivia Game</Text>
+    </View>
+  )
+}
+
+export default Header
