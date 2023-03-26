d5048074c2e70b7d5d3a6bf42fcb1bf895f6c115
Elmer Galo <elmer.galo@outlook.com>
2023-03-25 23:16:52 -0400

    add custom splash screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
+import React, { useEffect } from "react";
+import * as SplashScreen from "expo-splash-screen";
 import useLoadFonts from "hooks/useLoadFonts";
 import Layout from "components/layout";
 import ReduceProviders from "components/reduce-providers";
 import { TriviaProvider } from "context/trivia";
 import { TranslateProvider } from "context/i18n";
 
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
   const { fontsLoaded } = useLoadFonts();
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      (async () => {
+        await SplashScreen.hideAsync();
+      })();
+    }
+  }, [fontsLoaded]);
 
   if (!fontsLoaded) {
     return null;
